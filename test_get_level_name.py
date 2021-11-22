22a720c63d0e9710e9e855c5b6679a66ef580f74
OceNick <ozcenick@gmail.com>
2021-11-21 23:07:58 -0800

    Implement the unittest for get_level_name function

--- test_get_level_name.py
+++ test_get_level_name.py
@@ -1,6 +1,19 @@
 from unittest import TestCase
+from game import get_level_name as get_level_name
 
 
 class Test(TestCase):
-    def test_get_level_name(self):
-        self.fail()
+
+    def test_get_level_name_level_is_not_three(self):
+        adeptus = "Adeptus Astra Telepathica"
+        level = 1
+        expected = [1, "Acolyte"]
+        actual = get_level_name(adeptus, level)
+        self.assertEqual(expected, actual)
+
+    def test_get_level_name_level_is_three(self):
+        adeptus = "Adeptus Astra Telepathica"
+        level = 3
+        expected = [3, "Magister Telepathicae"]
+        actual = get_level_name(adeptus, level)
+        self.assertEqual(expected, actual)
