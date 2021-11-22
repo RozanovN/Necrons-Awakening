e2370dc3c619d21f28da3a648e3daafaf0442910
OceNick <ozcenick@gmail.com>
2021-11-22 04:12:17 -0800

    Implement the unittest for set_name function

--- test_set_name.py
+++ test_set_name.py
@@ -1,6 +1,13 @@
+import builtins
 from unittest import TestCase
+from game import set_name as set_name
+from unittest.mock import patch
 
 
 class Test(TestCase):
-    def test_set_name(self):
-        self.fail()
+
+    @patch('builtins.input', return_value="abc")
+    def test_set_name_return_is_valid(self, _):
+        expected = "Abc"
+        actual = set_name()
+        self.assertEqual(expected, actual)
