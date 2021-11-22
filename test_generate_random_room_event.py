62370b00a046aedc00f633dd4c4b2518d8e32ad1
OceNick <ozcenick@gmail.com>
2021-11-21 22:48:07 -0800

    Implement the unittest for generate_random_room_event function

--- test_generate_random_room_event.py
+++ test_generate_random_room_event.py
@@ -1,6 +1,12 @@
 from unittest import TestCase
+from game import generate_random_room_event as generate_random_room_event
+from unittest.mock import patch
 
 
 class Test(TestCase):
-    def test_generate_random_room_event(self):
-        self.fail()
+
+    @patch('random.choices', return_value=["Shifting Mist"])
+    def test_generate_random_room_event(self, _):
+        expected = "Shifting Mist"
+        actual = generate_random_room_event()
+        self.assertEqual(expected, actual)
