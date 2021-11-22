4fd7dcd3813a6aed470d2b22b431505dac3703b7
OceNick <ozcenick@gmail.com>
2021-11-22 03:35:17 -0800

    Delete untestable unittests test_event_with_check_of_item.py and test_process_command.py

--- test_event_with_check_of_item.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class Test(TestCase):
-    def test_event_with_check_of_item(self):
-        self.fail()
--- test_process_command.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class Test(TestCase):
-    def test_process_command(self):
-        self.fail()
