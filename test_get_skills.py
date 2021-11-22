dfe2b6688debae0941c5304c964113a05cf84360
OceNick <ozcenick@gmail.com>
2021-11-21 23:15:54 -0800

    Implement the unittest for get_skills function

--- test_get_skills.py
+++ test_get_skills.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
+from game import get_skills as get_skills
 
 
 class Test(TestCase):
-    def test_get_skills(self):
-        self.fail()
+
+    def test_get_command_list(self):
+        character = {"Skills": {}, "Adeptus": "Adeptus Astra Telepathica"}
+        expected = {"Skills": {"Lightning": "A bolt of blinding lightning strikes from your hand dealing 2k10 damage."},
+                    "Adeptus": "Adeptus Astra Telepathica"}
+        get_skills(character)
+        actual = character
+        self.assertEqual(expected, actual)
