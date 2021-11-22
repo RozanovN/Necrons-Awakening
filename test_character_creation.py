7790fb22df7f0840afe5b5f8535552bcac9d46a0
OceNick <ozcenick@gmail.com>
2021-11-21 21:09:50 -0800

    Implement the unittest for character_creation function

--- test_character_creation.py
+++ test_character_creation.py
@@ -1,26 +1,28 @@
 from unittest import TestCase
 from game import character_creation as character_creation
 from unittest.mock import patch
-import io
 
 
 class Test(TestCase):
 
     @patch('game.set_name', return_value="Horus")
     @patch('game.set_adeptus', return_value="Adeptus Mechanicus")
-    @patch('game.get_skills', return_value="")
+    @patch('game.set_wounds', return_value=25)
+    @patch('game.get_characteristics', return_value={"Intellect": 50, "Strength": 42, "Toughness": 47, "Agility": 39})
+    @patch('game.get_level_name', return_value=(1, "Acolyte"))
     @patch('builtins.input', return_value="1")
-    def test_character_creation_test_return(self, _, __, ___):
+    def test_character_creation_test_whether_valid_character_returned(self, _, __, ___, ____, _____, ______):
         expected = {
             "Name": "Horus",
             "Adeptus": "Adeptus Mechanicus",
             "Max wounds": 25,
             "Current wounds": 25,
             "Characteristics": {"Intellect": 50, "Strength": 42, "Toughness": 47, "Agility": 39},
-            "Level": [1, "Acolyte"],
+            "Level": (1, "Acolyte"),
             "Skills": {
                 "Flee Away": "You retreat to the previous room.",
-                "Laser Shot": "Lorem"
+                "Laser Shot": "Your servo-skull shots a laser beam from its eyes "
+                              "dealing (3 + Intellect Bonus) damage."
             },
             "X-coordinate": 0,
             "Y-coordinate": 0,
