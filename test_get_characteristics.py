7ede9730cdbc8124943a3cf2998471a867c0370d
OceNick <ozcenick@gmail.com>
2021-11-21 23:01:42 -0800

    Implement the unittest for get_characteristics function

--- test_get_characteristics.py
+++ test_get_characteristics.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
+from game import get_characteristics as get_characteristics
+from unittest.mock import patch
 
 
 class Test(TestCase):
-    def test_get_characteristics(self):
-        self.fail()
 
-
-class Test(TestCase):
-    def test_get_characteristics(self):
-        self.fail()
+    @patch('builtins.input', return_value="1")
+    @patch('game.roll', return_value=5)
+    def test_get_characteristics_return_is_valid(self, _, __):
+        character = {"Name": "Char", "Adeptus": "Adeptus Astra Telepathica"}
+        expected = {"Intellect": 35, "Strength": 35, "Toughness": 35, "Agility": 35}
+        actual = get_characteristics(character)
+        self.assertEqual(expected, actual)
