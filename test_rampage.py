1f91e6fa019b80849a6579206ea565b29aa999b9
OceNick <ozcenick@gmail.com>
2021-11-22 04:00:30 -0800

    Implement the unittest for rampage function

--- test_rampage.py
+++ test_rampage.py
@@ -1,6 +1,25 @@
 from unittest import TestCase
+from game import rampage as rampage
+from unittest.mock import patch
+import io
 
 
 class Test(TestCase):
-    def test_rampage(self):
-        self.fail()
+
+    @patch('game.roll', return_value=5)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_rampage_print_statement(self, mock_stdout, _):
+        character = {"Name": "Char", "Characteristics": {"Strength": 40}}
+        enemy = {"Name": "Test"}
+        expected = "\nChar makes a series of bloodthirsty slashes dealing 5 damage to Test.\n"
+        rampage(character, enemy)
+        actual = mock_stdout.getvalue()
+        self.assertEqual(expected + "\n", actual)
+
+    @patch('game.roll', return_value=5)
+    def test_rampage_return_value(self, _):
+        character = {"Name": "Char", "Characteristics": {"Strength": 40}}
+        enemy = {"Name": "Test"}
+        expected = 5
+        actual = rampage(character, enemy)
+        self.assertEqual(expected, actual)
