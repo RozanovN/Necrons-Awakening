8c11115b82531a096ffb5a41342849be2fa669a7
OceNick <ozcenick@gmail.com>
2021-11-21 23:47:50 -0800

    Implement the unittest for laser_shot function

--- test_laser_shot.py
+++ test_laser_shot.py
@@ -1,6 +1,23 @@
 from unittest import TestCase
+from game import laser_shot as laser_shot
+from unittest.mock import patch
+import io
 
 
 class Test(TestCase):
-    def test_laser_shot(self):
-        self.fail()
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_laser_shot_print_statement(self, mock_stdout):
+        character = {"Name": "Char", "Characteristics": {"Intellect": 40}}
+        enemy = {"Name": "Test"}
+        expected = "\nYour servo-skull shots a laser beam from its eyes dealing 7 damage to Test.\n"
+        laser_shot(character, enemy)
+        actual = mock_stdout.getvalue()
+        self.assertEqual(expected + "\n", actual)
+
+    def test_laser_shot_return_value(self):
+        character = {"Name": "Char", "Characteristics": {"Intellect": 40}}
+        enemy = {"Name": "Test"}
+        expected = 7
+        actual = laser_shot(character, enemy)
+        self.assertEqual(expected, actual)
