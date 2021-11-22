ced1bac230eaba06c7dc5c6abe2c74f1f1204762
OceNick <ozcenick@gmail.com>
2021-11-21 23:18:57 -0800

    Implement the unittest for has_argument function

--- test_has_argument.py
+++ test_has_argument.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
+from game import has_argument as has_argument
 
 
 class Test(TestCase):
-    def test_has_argument(self):
-        self.fail()
+
+    def test_has_argument_command_has_no_argument(self):
+        expected = False
+        actual = has_argument("h")
+        self.assertEqual(expected, actual)
+
+    def test_has_argument_command_has_argument(self):
+        expected = True
+        actual = has_argument("b")
+        self.assertEqual(expected, actual)
