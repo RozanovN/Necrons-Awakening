363cb8d35b3a432b453e586579e73d6737468379
OceNick <ozcenick@gmail.com>
2021-11-22 04:23:31 -0800

    Implement the unittest for set_wounds function

--- test_set_wounds.py
+++ test_set_wounds.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
+from game import set_wounds as set_wounds
+from unittest.mock import patch
 
 
 class Test(TestCase):
-    def test_set_wounds(self):
-        self.fail()
+
+    @patch('game.roll', return_value=5)
+    @patch('builtins.input', return_value=5)
+    def test_set_wounds_return_is_valid(self, _, __):
+        expected = 22
+        actual = set_wounds(character={"Name": "Test", "Adeptus": "Adeptus Astra Telepathica"})
+        self.assertEqual(expected, actual)
