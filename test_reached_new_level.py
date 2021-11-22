ce5adafbfa415e66b31f957478a3cb291fa2a988
OceNick <ozcenick@gmail.com>
2021-11-22 04:05:38 -0800

    Implement the unittest for reached_new_level function

--- test_reached_new_level.py
+++ test_reached_new_level.py
@@ -1,6 +1,25 @@
 from unittest import TestCase
+from game import reached_new_level as reached_new_level
 
 
 class Test(TestCase):
-    def test_reached_new_level(self):
-        self.fail()
+
+    def test_reached_new_level_level_three(self):
+        character = {"Name": "Char", "Characteristics": {"Agility": 40}, 'Inventory': {}, "Level": [3, "abc"]}
+        expected = False
+        actual = reached_new_level(character)
+        self.assertEqual(expected, actual)
+
+    def test_reached_new_level_level_not_three_but_reached_new_level(self):
+        character = {"Name": "Char", "Characteristics": {"Agility": 40}, 'Inventory': {}, "Level": [2, "abc"],
+                     "Current experience": 1000, "Experience for the next level": 500}
+        expected = True
+        actual = reached_new_level(character)
+        self.assertEqual(expected, actual)
+
+    def test_reached_new_level_level_not_three_but_not_reached_new_level(self):
+        character = {"Name": "Char", "Characteristics": {"Agility": 40}, 'Inventory': {}, "Level": [2, "abc"],
+                     "Current experience": 1000, "Experience for the next level": 1500}
+        expected = False
+        actual = reached_new_level(character)
+        self.assertEqual(expected, actual)
