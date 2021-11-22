9191467448981169215216cf33fb57b16db9eb75
OceNick <ozcenick@gmail.com>
2021-11-21 23:30:03 -0800

    Implement the unittest for has_item function

--- test_has_item.py
+++ test_has_item.py
@@ -1,6 +1,17 @@
 from unittest import TestCase
+from game import has_item as has_item
 
 
 class Test(TestCase):
-    def test_has_item(self):
-        self.fail()
+
+    def test_has_item_no_item(self):
+        character = {"Name": "Char", "Inventory": {"Bandage": 5}}
+        expected = False
+        actual = has_item("Torch", character)
+        self.assertEqual(expected, actual)
+
+    def test_has_item_item_is_present(self):
+        character = {"Name": "Char", "Inventory": {"Shovel": 5}}
+        expected = True
+        actual = has_item("Shovel", character)
+        self.assertEqual(expected, actual)
