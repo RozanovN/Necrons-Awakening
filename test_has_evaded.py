5496facb0750db56a515438614701ff880c03a68
OceNick <ozcenick@gmail.com>
2021-11-21 23:27:40 -0800

    Fix typo

--- test_has_evaded.py
+++ test_has_evaded.py
@@ -17,14 +17,14 @@ class Test(TestCase):
         self.assertEqual(expected + "\n", actual)
 
     @patch('game.roll', return_value=100)
-    def test_has_evaded_unsuccessful_return_value_(self, _):
+    def test_has_evaded_unsuccessful_return_value(self, _):
         character = {"Name": "Char", "Characteristics": {"Intellect": 40, "Agility": 60}}
         expected = False
         actual = has_evaded(character)
         self.assertEqual(expected, actual)
 
     @patch('game.roll', return_value=5)
-    def test_has_evaded_successful_return_value_(self, _):
+    def test_has_evaded_successful_return_value(self, _):
         character = {"Name": "Char", "Characteristics": {"Intellect": 40, "Agility": 60}}
         expected = True
         actual = has_evaded(character)
