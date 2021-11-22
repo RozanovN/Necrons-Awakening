e74b75d5bdd97570be9d584787c2689937b228c6
OceNick <ozcenick@gmail.com>
2021-11-21 22:45:09 -0800

    Implement the unittest for generate_enemy function

--- test_generate_enemy.py
+++ test_generate_enemy.py
@@ -1,6 +1,51 @@
 from unittest import TestCase
+from game import generate_enemy as generate_enemy
+from unittest.mock import patch
 
 
 class Test(TestCase):
-    def test_generate_enemy(self):
-        self.fail()
+
+    @patch('random.choices', return_value=["the rat"])
+    def test_generate_enemy_random_enemy(self, _):
+        character = {"Level": (1, "Acolyte")}
+        expected = {
+                "Name": "the rat",
+                "Max wounds": 5,
+                "Current wounds": 5,
+                "Characteristics": {
+                    "Intellect": 10,
+                    "Strength": 15,
+                    "Toughness": 15,
+                    "Agility": 25
+                },
+                "Skills": {
+                    "Flee Away": "The rat flees away.",
+                    "Enemy Attack": ("The rat greedily bites you with its front teeth", 1, 5)
+                },
+                "Will to fight": True,
+                "Experience": 150
+            }
+        actual = generate_enemy(character["Level"][0])
+        self.assertEqual(expected, actual)
+
+    def test_generate_enemy_specific_enemy(self):
+        character = {"Level": (1, "Acolyte")}
+        expected = {
+                "Name": "the rat",
+                "Max wounds": 5,
+                "Current wounds": 5,
+                "Characteristics": {
+                    "Intellect": 10,
+                    "Strength": 15,
+                    "Toughness": 15,
+                    "Agility": 25
+                },
+                "Skills": {
+                    "Flee Away": "The rat flees away.",
+                    "Enemy Attack": ("The rat greedily bites you with its front teeth", 1, 5)
+                },
+                "Will to fight": True,
+                "Experience": 150
+            }
+        actual = generate_enemy(character["Level"][0], "the rat")
+        self.assertEqual(expected, actual)
