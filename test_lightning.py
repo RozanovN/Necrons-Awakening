0234b43871a8bb45f8c6d110935b910afbc470bc
OceNick <ozcenick@gmail.com>
2021-11-22 02:46:30 -0800

    Implement the unittest for lightning function

--- test_lightning.py
+++ test_lightning.py
@@ -1,6 +1,25 @@
 from unittest import TestCase
+from game import lightning as lightning
+from unittest.mock import patch
+import io
 
 
 class Test(TestCase):
-    def test_lightning(self):
-        self.fail()
+
+    @patch('game.roll', return_value=5)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_lightning_print_statement(self, mock_stdout, _):
+        character = {"Name": "Char", "Characteristics": {"Agility": 40}}
+        enemy = {"Name": "Test"}
+        expected = "\nA bolt of blinding lightning strikes from Char's hand dealing 5 damage to Test."
+        lightning(character, enemy)
+        actual = mock_stdout.getvalue()
+        self.assertEqual(expected + "\n", actual)
+
+    @patch('game.roll', return_value=5)
+    def test_lightning_return_value(self, _):
+        character = {"Name": "Char", "Characteristics": {"Agility": 40}}
+        enemy = {"Name": "Test"}
+        expected = 5
+        actual = lightning(character, enemy)
+        self.assertEqual(expected, actual)
