3ac15ecbc68503e15beb84aa0dd2a6011664792e
OceNick <ozcenick@gmail.com>
2021-11-22 03:33:47 -0800

    Implement the unittest for event_with_input function

--- test_event_with_input.py
+++ test_event_with_input.py
@@ -1,6 +1,32 @@
 from unittest import TestCase
+from game import event_with_input as event_with_input
+from unittest.mock import patch
+import io
 
 
 class Test(TestCase):
-    def test_event_with_input(self):
-        self.fail()
+
+    @patch('game.event_effect_or_item', return_value=1)
+    @patch('game.print_numbered_list_of_possibilities', return_value=1)
+    @patch('game.process_input', return_value=2)
+    @patch('game.has_item', return_value=False)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_event_with_check_of_item_description(self, mock_stdout, _, __, ___, ____):
+        character = {"Name": "Char", "Characteristics": {"Agility": 40}, 'Inventory': {}}
+        expected = "abc"
+        event = {"Input": {"Description": "abc", "No": "abc"}}
+        event_with_input(character, event)
+        actual = mock_stdout.getvalue()
+        self.assertEqual(expected + "\n", actual)
+
+    @patch('game.event_effect_or_item', return_value=1)
+    @patch('game.print_numbered_list_of_possibilities', return_value=1)
+    @patch('game.process_input', return_value=1)
+    @patch('game.has_item', return_value=False)
+    def test_event_with_check_of_item_after_effect(self, _, __, ___, ____):
+        character = {"Name": "Char", "Characteristics": {"Agility": 40}, 'Inventory': {}}
+        expected = {"Input": {"Description": "abc", "Yes": "abc"}, "After-effect": True}
+        event = {"Input": {"Description": "abc", "Yes": "abc"}, "After-effect": False}
+        event_with_input(character, event)
+        actual = event
+        self.assertEqual(expected, actual)
