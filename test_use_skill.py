bbb9066cbbf15e86671f32bcec6d8f1f38b90d71
OceNick <ozcenick@gmail.com>
2021-11-22 04:28:23 -0800

    Implement the unittest for use_skill function

--- test_use_skill.py
+++ test_use_skill.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
+from game import use_skill as use_skill
+from unittest.mock import patch
 
 
 class Test(TestCase):
-    def test_use_skill(self):
-        self.fail()
+
+    @patch('game.spontaneous_combustion', return_value=5)
+    def test_use_skill_return_value(self, _):
+        character = {"Name": "Char", "Characteristics": {"Intellect": 40}}
+        enemy = {"Name": "Test"}
+        expected = 5
+        actual = use_skill(character, "Spontaneous Combustion", enemy)
+        self.assertEqual(expected, actual)
