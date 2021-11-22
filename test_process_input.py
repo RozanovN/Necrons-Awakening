78406da0a7c33216e85cfd68d8f4ff37974c9a11
OceNick <ozcenick@gmail.com>
2021-11-22 03:58:13 -0800

    Implement the unittest for process_input function

--- test_process_input.py
+++ test_process_input.py
@@ -1,6 +1,16 @@
 from unittest import TestCase
+from game import process_input as process_input
+from unittest.mock import patch
+import io
 
 
 class Test(TestCase):
-    def test_process_input(self):
-        self.fail()
+
+    @patch('game.validate_option', return_value=True)
+    @patch('builtins.input', return_value="1")
+    def test_event_with_check_of_item_description(self, _, __):
+        character = {"Name": "Char", "Characteristics": {"Agility": 40}, 'Inventory': {}}
+        expected = "1"
+        list_of_options = ["test", "test"]
+        actual = process_input(character, list_of_options)
+        self.assertEqual(expected, actual)
