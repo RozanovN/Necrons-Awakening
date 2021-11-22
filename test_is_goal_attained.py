f5f0e2dbb009328cd86823d246711a24c3736d0b
OceNick <ozcenick@gmail.com>
2021-11-21 23:39:51 -0800

    Implement the unittest for is_goal_attained function

--- test_is_goal_attained.py
+++ test_is_goal_attained.py
@@ -1,6 +1,17 @@
 from unittest import TestCase
+from game import is_goal_attained as is_goal_attained
 
 
 class Test(TestCase):
-    def test_is_goal_attained(self):
-        self.fail()
+
+    def test_is_goal_attained_goal_is_not_attained(self):
+        expected = False
+        character = {}
+        actual = is_goal_attained(character)
+        self.assertEqual(expected, actual)
+
+    def test_is_goal_attained_goal_is_attained(self):
+        expected = True
+        character = {"Artifact": "anything"}
+        actual = is_goal_attained(character)
+        self.assertEqual(expected, actual)
