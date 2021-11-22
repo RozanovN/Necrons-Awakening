8f331842800679ec1a0aeb6761ddebcecd46cbf5
Nikolay Rozanov <ozcenick@gmail.com>
2021-11-21 19:32:11 -0800

    Implement the additional unit test for chaos_of_warp function

--- test_chaos_of_warp.py
+++ test_chaos_of_warp.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from game import chaos_of_warp as chaos_of_warp
+from unittest.mock import patch
+import io
 
 
 class Test(TestCase):
@@ -13,3 +15,13 @@ class Test(TestCase):
         chaos_of_warp(character, enemy)
         actual = enemy
         self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_chaos_of_warp_enemy_print_statement(self, mock_stdout):
+        character = {"Name": "Test", "Current wounds": 1, "Max wounds": 1}
+        enemy = {"Name": "Goreclaw the Render, a Daemon Prince of Khorne", "Current wounds": 100,
+                 "Max wounds": 100}
+        expected = "\nYou make wounds of Goreclaw the Render, a Daemon Prince of Khorne equal to 1.\n"
+        chaos_of_warp(character, enemy)
+        actual = mock_stdout.getvalue()
+        self.assertEqual(expected + "\n", actual)
