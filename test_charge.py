ef38760fe5cea0ab7374f46f1170ce27192867ab
Nikolay Rozanov <ozcenick@gmail.com>
2021-11-21 19:25:24 -0800

    Fixed a semantic error

--- test_charge.py
+++ test_charge.py
@@ -6,9 +6,8 @@ import io
 
 class Test(TestCase):
 
-    @patch('game.roll', return_value=5)
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_charge_print_statement(self, mock_stdout, _):
+    def test_charge_print_statement(self, mock_stdout):
         character = {"Name": "Char", "Characteristics": {"Strength": 40}}
         enemy = {"Name": "Test"}
         expected = "\nChar's enormous body charges into Test dealing 12 damage\n"
@@ -16,8 +15,7 @@ class Test(TestCase):
         actual = mock_stdout.getvalue()
         self.assertEqual(expected + "\n", actual)
 
-    @patch('game.roll', return_value=5)
-    def test_charge_return_value(self, _):
+    def test_charge_return_value(self):
         character = {"Name": "Char", "Characteristics": {"Strength": 40}}
         enemy = {"Name": "Test"}
         expected = 12
