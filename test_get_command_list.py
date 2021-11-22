41529856a6822726caa9b8b86d0f089902b85d81
OceNick <ozcenick@gmail.com>
2021-11-21 23:03:23 -0800

    Implement the unittest for get_command_list function

--- test_get_command_list.py
+++ test_get_command_list.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
+from game import get_command_list as get_command_list
 
 
 class Test(TestCase):
     def test_get_command_list(self):
-        self.fail()
+
+        expected = ["q", "h", "b", "s", "i", "c"]
+        actual = get_command_list()
+        self.assertEqual(expected, actual)
