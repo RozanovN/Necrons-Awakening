d932204f608f2ce3d4764b13fbaea65cade6d8e9
OceNick <ozcenick@gmail.com>
2021-11-21 23:35:39 -0800

    Implement the unittest for has_sustained function

--- test_has_sustained.py
+++ test_has_sustained.py
@@ -1,6 +1,31 @@
 from unittest import TestCase
+from game import has_sustained as has_sustained
+from unittest.mock import patch
+import io
 
 
 class Test(TestCase):
-    def test_has_sustained(self):
-        self.fail()
+
+    @patch('game.roll', return_value=5)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_has_sustained_check_print_statement(self, mock_stdout, _):
+        character = {"Name": "Char", "Characteristics": {"Intellect": 40, "Toughness": 60}}
+        expected = "[1;32m----------------------------------Toughness Check-------------------------------" \
+                   "----------------------[0;20m"
+        has_sustained(character)
+        actual = mock_stdout.getvalue()
+        self.assertEqual(expected + "\n", actual)
+
+    @patch('game.roll', return_value=100)
+    def test_has_sustained_unsuccessful_return_value(self, _):
+        character = {"Name": "Char", "Characteristics": {"Intellect": 40, "Toughness": 60}}
+        expected = False
+        actual = has_sustained(character)
+        self.assertEqual(expected, actual)
+
+    @patch('game.roll', return_value=5)
+    def test_has_sustained_successful_return_value(self, _):
+        character = {"Name": "Char", "Characteristics": {"Intellect": 40, "Toughness": 60}}
+        expected = True
+        actual = has_sustained(character)
+        self.assertEqual(expected, actual)
